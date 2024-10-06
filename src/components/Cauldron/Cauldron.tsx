4edadc35e24a142dcdaef345987fe56edc8a214c
sergey.butov <zitrix.box@gmail.com>
2024-10-06 19:03:20 +0300

    nice cauldron on CSS (CPU usage is 9–15%, core i7)

--- src/components/Cauldron/Cauldron.tsx
+++ src/components/Cauldron/Cauldron.tsx
@@ -1,7 +1,35 @@
 import { observer } from "mobx-react-lite";
 import { RootStore, StoreContext, useContext } from "../../Contexts";
+import Delayed from "../Delayed";
 
 import "./Cauldron.css";
+
+const CauldronWave = observer(({left, top}: {left:string,top:string}) => {
+    return <>
+        <div className="cauldron-waves" style={{left,top}}>
+            <div className="cauldron-wave-arc cauldron-wave-a"></div>
+            <div className="cauldron-wave-arc cauldron-wave-b"></div>
+            <div className="cauldron-wave-arc cauldron-wave-c"></div>
+            <div className="cauldron-wave-arc cauldron-wave-d"></div>
+            <div className="cauldron-wave-arc cauldron-wave-e"></div>
+        </div>
+    </>
+});
+
+const CauldronIcon = observer(({i, mdIcon}: {i: number, mdIcon: string})=>{
+    return <>
+        <div className={`cauldron-icon cauldron-icon-${i}`}>
+            <div className="cauldron-icon-container">
+                <div className="cauldron-icon-under-the-sea">
+                    <span className="material-symbols-outlined">{mdIcon}</span>
+                </div>
+                <div className="cauldron-icon-over-the-sea">
+                    <span className="material-symbols-outlined">{mdIcon}</span>
+                </div>
+            </div>
+        </div>
+    </>
+})
 
 const Cauldron = observer(() => {
     // TODO: https://codepen.io/z-/pen/zYxdRQy
@@ -10,27 +38,25 @@ const Cauldron = observer(() => {
     return <div 
         className="cauldron"
     >
-        <div className="cauldron-body-top-back">
-            <div className="cauldron-body-left-1"></div>
-            <div className="cauldron-body-right-1"></div>
-            <div className="cauldron-body-center-1"></div>
-            <div className="cauldron-body-arc-1">
-            </div>
-            <div className="cauldron-body-arc-2">
-                <div className="cauldron-left-shadow"></div>
-            </div>
+        <div className="cauldron-rotate">
+            <div className="cauldron-bg"></div>
+            <div className="cauldron-border"></div>
         </div>
-        <div className="cauldron-body-bottom">
-            <div className="cauldron-body-left-1"></div>
-            <div className="cauldron-body-right-1"></div>
-            <div className="cauldron-body-bottom-1"></div>
-            <div className="cauldron-body-middle-1"></div>
-        </div>
-        <div className="cauldron-surface"></div>
-        <div className="cauldron-body-top-front">
-            <div className="cauldron-body-arc-1"></div>
-            <div className="cauldron-body-arc-2"></div>
-        </div>
+        <Delayed delay={11} key="wave-1">
+            <CauldronWave left="100px" top="100px" />
+        </Delayed>
+        <Delayed delay={111} key="wave-2">
+            <CauldronWave left="300px" top="0px" />
+        </Delayed>
+        <Delayed delay={202} key="wave-3">
+            <CauldronWave left="500px" top="50px" />
+        </Delayed>
+        <Delayed delay={313} key="wave-4">
+            <CauldronWave left="400px" top="120px" />
+        </Delayed>
+        <CauldronIcon i={0} mdIcon={"Air"} key="i0" />
+        <CauldronIcon i={1} mdIcon={"fire_truck"} key="i1" />
+        <CauldronIcon i={2} mdIcon={"Microbes"} key="i2" />
         555
     </div>;
 });
